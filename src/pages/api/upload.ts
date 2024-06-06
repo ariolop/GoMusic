69867c463fa75249f4bc6f0480b5f7eadd4d73b2
Alejandro <ariolop154@gmail.com>
2024-06-06 13:46:23 +0200

    Subida de ficheros

---
+++ src/pages/api/upload.ts
@@ -0,0 +1,14 @@
+import { put } from "@vercel/blob";
+import type { APIContext } from "astro";
+import { log } from "console";
+
+export async function POST(context: APIContext): Promise<Response> {
+  //const formData = await context.request.formData();
+  const blob = await context.request.blob()
+  const file = new File([blob], "ejemplo");
+
+  const { url } = await put(file.name, 'Hello World!', { access: 'public' });
+
+  return new Response(url, {status: 404})
+  //const { url } = await put('articles/blob.txt', 'Hello World!', { access: 'public' });
+}
