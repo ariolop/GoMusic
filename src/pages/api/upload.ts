013662aed0153205e9e54cf5e06b28441b249daf
Alejandro <ariolop154@gmail.com>
2024-06-06 18:05:35 +0200

    Fix upload music

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -3,11 +3,10 @@ import type { APIContext } from "astro";
 import { log } from "console";
 
 export async function POST(context: APIContext): Promise<Response> {
-  //const formData = await context.request.formData();
-  const blob = await context.request.blob()
-  const file = new File([blob], "ejemplo.mp3");
+  const form = await context.request.formData();
+  const file = form.get('file') as File;
 
-  const { url } = await put(file.name, 'Hello World!', { access: 'public' });
+  const { url } = await put(file.name, file, { access: 'public' });
 
   return new Response(url, {status: 404})
   //const { url } = await put('articles/blob.txt', 'Hello World!', { access: 'public' });
