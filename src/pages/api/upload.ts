456ece810aec3dc01dc21772bc9478f278afaab4
Alejandro <ariolop154@gmail.com>
2024-06-06 18:01:08 +0200

    Fix upload music

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -5,7 +5,7 @@ import { log } from "console";
 export async function POST(context: APIContext): Promise<Response> {
   //const formData = await context.request.formData();
   const blob = await context.request.blob()
-  const file = new File([blob], "ejemplo");
+  const file = new File([blob], "ejemplo.mp3");
 
   const { url } = await put(file.name, 'Hello World!', { access: 'public' });
 
