1f02421b1c0dac35eeb18f4b4bd97a7506c29e25
Alejandro <ariolop154@gmail.com>
2024-06-10 11:37:38 +0200

    ...

--- src/pages/api/uploadMusic.ts
+++ src/pages/api/uploadMusic.ts
@@ -20,7 +20,12 @@ export async function POST(context: APIContext): Promise<Response> {
         return context.redirect("/insertarAudio?datos=faltan")
     }
 
+    console.log("Empezamos el try");
+    
+
     try {
+        console.log("Obtenemos el userId y el nombre completo del artista");
+        
         //Obtenemos el userId y el nombre completo del artista
         const sessionId = context.cookies.get("auth_session").value
         const userId = (await db.select({userId: Session.userId})
@@ -36,16 +41,39 @@ export async function POST(context: APIContext): Promise<Response> {
         const nombreAlbum = (await db.select({nombreAlbum: Album.nombreAlbum}).from(Album).where(eq(Album.idAlbum, inputAlbum.toString())))[0].nombreAlbum
         const hoy = Date.now()
 
+        console.log("Obtenemos todos los campos de la tabla Audios");
+
         //Obtenemos todos los campos de la tabla Audios
         const idAudio = generateId(9)
+        console.log(idAudio);
+        
         const nombreAudio = inputTitulo.toString()
-        const descripcion = inputDescripcion.toString()
+        console.log(nombreAudio);
+
+        const descripcion = inputDescripcion ? inputDescripcion.toString() : ""
+        console.log(descripcion);
+
         const genero = inputGenero.toString()
-        const tipo = inputTitulo.toString()
+        console.log(genero);
+
+        const tipo = inputTipo.toString()
+        console.log(tipo);
+
+
         const { url: urlImage } = await put("portadas/" + nombreArtista + "/" + nombreAlbum + "/" + inputPortada.name, inputPortada, { access: 'public' });
+        console.log(urlImage);
+        
         const { url: urlAudio } = await put("audios/" + nombreArtista + "/" + nombreAlbum + "/" + inputAudio.name, inputAudio, { access: 'public' });
+        console.log(urlAudio);
+
         const subidoEn = new Date(hoy)
-        const autores_secundarios = inputAutoresSecundarios.toString()
+        console.log(subidoEn);
+
+        const autores_secundarios = inputAutoresSecundarios ? inputAutoresSecundarios.toString() : ""
+        console.log(autores_secundarios);
+        
+
+        console.log("Insertamos el registro en la tabla Audios");
 
         //Insertamos el registro en la tabla Audios
         await db.insert(Audios).values([
@@ -79,7 +107,7 @@ export async function POST(context: APIContext): Promise<Response> {
     }
     catch
     {
-        return new Response("Algo falló", {status: 404})
+        return new Response("Algo falló", {status: 500})
     }
 
 }
