f1f6ffedc48941223f21edf677a93ea343fba509
Alejandro <ariolop154@gmail.com>
2024-06-10 11:46:22 +0200

    ...

--- src/pages/api/uploadMusic.ts
+++ src/pages/api/uploadMusic.ts
@@ -23,91 +23,85 @@ export async function POST(context: APIContext): Promise<Response> {
     console.log("Empezamos el try");
     
 
-    try {
-        console.log("Obtenemos el userId y el nombre completo del artista");
-        
-        //Obtenemos el userId y el nombre completo del artista
-        const sessionId = context.cookies.get("auth_session").value
-        const userId = (await db.select({userId: Session.userId})
-                                .from(Session)
-                                .where(eq(Session.id, sessionId)))[0].userId
+    console.log("Obtenemos el userId y el nombre completo del artista");
+    
+    //Obtenemos el userId y el nombre completo del artista
+    const sessionId = context.cookies.get("auth_session").value
+    const userId = (await db.select({userId: Session.userId})
+                            .from(Session)
+                            .where(eq(Session.id, sessionId)))[0].userId
 
-        const nombreCompletoArtista = (await db.select({nombre: Usuario.nombre, apellidos: Usuario.apellidos})
-                                                .from(Usuario)  
-                                                .where(eq(Usuario.id, userId)))[0]
+    const nombreCompletoArtista = (await db.select({nombre: Usuario.nombre, apellidos: Usuario.apellidos})
+                                            .from(Usuario)  
+                                            .where(eq(Usuario.id, userId)))[0]
 
-        const nombreArtista = nombreCompletoArtista.nombre + "_" + nombreCompletoArtista.apellidos                                        
+    const nombreArtista = nombreCompletoArtista.nombre + "_" + nombreCompletoArtista.apellidos                                        
 
-        const nombreAlbum = (await db.select({nombreAlbum: Album.nombreAlbum}).from(Album).where(eq(Album.idAlbum, inputAlbum.toString())))[0].nombreAlbum
-        const hoy = Date.now()
+    const nombreAlbum = (await db.select({nombreAlbum: Album.nombreAlbum}).from(Album).where(eq(Album.idAlbum, inputAlbum.toString())))[0].nombreAlbum
+    const hoy = Date.now()
 
-        console.log("Obtenemos todos los campos de la tabla Audios");
+    console.log("Obtenemos todos los campos de la tabla Audios");
 
-        //Obtenemos todos los campos de la tabla Audios
-        const idAudio = generateId(9)
-        console.log(idAudio);
-        
-        const nombreAudio = inputTitulo.toString()
-        console.log(nombreAudio);
+    //Obtenemos todos los campos de la tabla Audios
+    const idAudio = generateId(9)
+    console.log(idAudio);
+    
+    const nombreAudio = inputTitulo.toString()
+    console.log(nombreAudio);
 
-        const descripcion = inputDescripcion ? inputDescripcion.toString() : ""
-        console.log(descripcion);
+    const descripcion = inputDescripcion ? inputDescripcion.toString() : ""
+    console.log(descripcion);
 
-        const genero = inputGenero.toString()
-        console.log(genero);
+    const genero = inputGenero.toString()
+    console.log(genero);
 
-        const tipo = inputTipo.toString()
-        console.log(tipo);
+    const tipo = inputTipo.toString()
+    console.log(tipo);
 
 
-        const { url: urlImage } = await put("portadas/" + nombreArtista + "/" + nombreAlbum + "/" + inputPortada.name, inputPortada, { access: 'public' });
-        console.log(urlImage);
-        
-        const { url: urlAudio } = await put("audios/" + nombreArtista + "/" + nombreAlbum + "/" + inputAudio.name, inputAudio, { access: 'public' });
-        console.log(urlAudio);
+    const { url: urlImage } = await put("portadas/" + nombreArtista + "/" + nombreAlbum + "/" + inputPortada.name, inputPortada, { access: 'public' });
+    console.log(urlImage);
+    
+    const { url: urlAudio } = await put("audios/" + nombreArtista + "/" + nombreAlbum + "/" + inputAudio.name, inputAudio, { access: 'public' });
+    console.log(urlAudio);
 
-        const subidoEn = new Date(hoy)
-        console.log(subidoEn);
+    const subidoEn = new Date(hoy)
+    console.log(subidoEn);
 
-        const autores_secundarios = inputAutoresSecundarios ? inputAutoresSecundarios.toString() : ""
-        console.log(autores_secundarios);
-        
+    const autores_secundarios = inputAutoresSecundarios ? inputAutoresSecundarios.toString() : ""
+    console.log(autores_secundarios);
+    
 
-        console.log("Insertamos el registro en la tabla Audios");
+    console.log("Insertamos el registro en la tabla Audios");
 
-        //Insertamos el registro en la tabla Audios
-        await db.insert(Audios).values([
-            {
-                idAudio,
-                nombreAudio,
-                descripcion,
-                genero,
-                tipo,
-                rutaImagen: urlImage,
-                rutaSonido: urlAudio,
-                subidoEn,
-                autores_secundarios
-            }
-        ])
+    //Insertamos el registro en la tabla Audios
+    await db.insert(Audios).values([
+        {
+            idAudio,
+            nombreAudio,
+            descripcion,
+            genero,
+            tipo,
+            rutaImagen: urlImage,
+            rutaSonido: urlAudio,
+            subidoEn,
+            autores_secundarios
+        }
+    ])
 
-        //Obtenemos todas los campos de la tabla Album_Audio
-        const idAlbumAudio = generateId(9)
-        const idAlbum = inputAlbum.toString()
+    //Obtenemos todas los campos de la tabla Album_Audio
+    const idAlbumAudio = generateId(9)
+    const idAlbum = inputAlbum.toString()
 
-        //Insertamos el registro en la tabla Album_Audio
-        await db.insert(Album_Audio).values([
-            {
-                idAlbumAudio,
-                idAlbum,
-                idAudio
-            }
-        ])
+    //Insertamos el registro en la tabla Album_Audio
+    // await db.insert(Album_Audio).values([
+    //     {
+    //         idAlbumAudio,
+    //         idAlbum,
+    //         idAudio
+    //     }
+    // ])
 
-        return context.redirect("/insertarAudio?insertar=correcto")
-    }
-    catch
-    {
-        return new Response("Algo falló", {status: 500})
-    }
+    return context.redirect("/insertarAudio?insertar=correcto")
 
 }
